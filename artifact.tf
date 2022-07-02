6240a0ff711444811db04ced473b1030e4039901
Italo Santos <italorsants@gmail.com>
2022-07-02 15:53:58 -0300

    alterando nomes

--- artifact.tf
+++ artifact.tf
@@ -2,7 +2,7 @@ resource "google_artifact_registry_repository" "my-repo" {
   provider = google-beta
 
   location = "us-central1"
-  repository_id = "spotmusic"
+  repository_id = "spotmusic-2"
   description = "Imagens Docker"
   format = "DOCKER"
 }
